cbcf8401161a50ae66c3118ccc50114d398aab9b
gregdenay <gregoire.denay@cvua-rrw.de>
2021-01-21 15:36:49 +0100

    Added test data

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' H1-NMR spectra of Egg yolk fatty-acids
+#' 
+#' A dataset containing H1-NMR fatty-acid spectra for 10 egg yolk samples, binned to 0.0004 ppm.
+#'
+#' @format A tibble with 2621440 rows and 3 variables:
+#' \describe{
+#'   \item{ID}{Unique sample identifier}
+#'   \item{bins}{Ppm value (x-axis)}
+#'   \item{values}{Intensity value}
+#' }
+"nmr_spectra"
